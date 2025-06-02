c5d54918733742b81c2ad3aba5a403611c463a4a
Niriksha Patel <nirikshapatel@Nirikshas-MBP.Dlink>
2025-06-02 18:03:29 +0530

    port change

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
+const PORT = 80;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}.`);
 });
