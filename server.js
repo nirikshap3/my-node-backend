a9d7a67a8b3fa8012d77042ec1e4b1469092b8b9
Niriksha Patel <nirikshapatel@Nirikshas-MBP.Dlink>
2025-06-02 18:44:54 +0530

    ngnix setup change

--- server.js
+++ server.js
@@ -38,8 +38,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-// const PORT = process.env.PORT || 8080;
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}.`);
 });
