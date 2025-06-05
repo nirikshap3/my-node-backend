27fb6dc478f077b0ac50918029bae967ea9f85e4
Niriksha Patel <nirikshapatel@Nirikshas-MBP.Dlink>
2025-06-05 18:57:23 +0530

    auth route fix

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,8 +3,12 @@ const authController = require("../controllers/auth.controller");
 module.exports = (app) => {
   const router = require("express").Router();
 
-  router.post("/register", authController.register);
-  router.post("/login", authController.login);
+//   router.post("/register", authController.register);
+//   router.post("/login", authController.login);
+
+router.get("/", (req, res) => {
+    res.send("Auth API working!");
+  });
 
   app.use("/api/auth", router);  // Now /api/auth/register and /api/auth/login are available
 };
